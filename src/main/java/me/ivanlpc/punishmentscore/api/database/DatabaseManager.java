72431df31abb33c42dc7d9953ce19c6c642d15cc
Ivanlpc <100932340+Ivanlpc@users.noreply.github.com>
2023-07-28 13:29:29 +0200

    change code to use try-with-resources

--- src/main/java/me/ivanlpc/punishmentscore/api/database/DatabaseManager.java
+++ src/main/java/me/ivanlpc/punishmentscore/api/database/DatabaseManager.java
@@ -17,31 +17,34 @@ public class DatabaseManager {
     }
     public int createOrder(Player p, String userPunished, String punishment, String reason, List<String> command) {
         int orderId = -1;
-        try (Connection conn = this.hikari.getConnection()) {
-            String query = "INSERT INTO orders (username, uuid, user_punished, punishment, reason) VALUES (?, ?, ?, ?, ?)";
-            PreparedStatement stmt = conn.prepareStatement(query, Statement.RETURN_GENERATED_KEYS);
+        String query = "INSERT INTO orders (username, uuid, user_punished, punishment, reason) VALUES (?, ?, ?, ?, ?)";
+
+        try (Connection conn = this.hikari.getConnection();
+             PreparedStatement stmt = conn.prepareStatement(query, Statement.RETURN_GENERATED_KEYS)) {
                 stmt.setString(1, p.getPlayerListName());
                 stmt.setString(2, p.getUniqueId().toString());
                 stmt.setString(3, userPunished);
                 stmt.setString(4, punishment);
                 stmt.setString(5, reason);
                 stmt.executeUpdate();
-                ResultSet generatedKeys = stmt.getGeneratedKeys();
-                if (generatedKeys.next()) {
-                    int id = generatedKeys.getInt(1);
-                    String insertQuery = "INSERT INTO commands (order_id, command) VALUES (?, ?)";
-                    try (PreparedStatement cmdStmt = conn.prepareStatement(insertQuery)) {
-                        for (String cmd : command) {
-                            cmdStmt.setInt(1, orderId);
-                            cmdStmt.setString(2, cmd);
-                            cmdStmt.addBatch();
+
+                try(ResultSet generatedKeys = stmt.getGeneratedKeys()) {
+                    if (generatedKeys.next()) {
+                        int id = generatedKeys.getInt(1);
+                        String insertQuery = "INSERT INTO commands (order_id, command) VALUES (?, ?)";
+                        try (PreparedStatement cmdStmt = conn.prepareStatement(insertQuery)) {
+                            for (String cmd : command) {
+                                cmdStmt.setInt(1, orderId);
+                                cmdStmt.setString(2, cmd);
+                                cmdStmt.addBatch();
+                            }
+                            cmdStmt.executeBatch();
                         }
-                        cmdStmt.executeBatch();
+                        Bukkit.getConsoleSender().sendMessage(ChatColor.AQUA + "Created new order: " + orderId);
+                        orderId = id;
+                    } else {
+                        throw new SQLException("Order ID not generated");
                     }
-                    Bukkit.getConsoleSender().sendMessage(ChatColor.AQUA + "Created new order: " + orderId);
-                    orderId = id;
-                } else {
-                    throw new SQLException("Order ID not generated");
                 }
         } catch (SQLException e) {
             e.printStackTrace();
@@ -49,27 +52,27 @@ public class DatabaseManager {
         return orderId;
     }
     public void createTable() {
-        try(Connection conn = this.hikari.getConnection()) {
-            PreparedStatement createOrders = conn.prepareStatement(
-                    "CREATE TABLE IF NOT EXISTS orders (" +
-                    "`id` int(11) NOT NULL AUTO_INCREMENT," +
-                    "`username` varchar(40) NOT NULL," +
-                    "`uuid` varchar(60) NOT NULL," +
-                    "`user_punished` varchar(40) NOT NULL," +
-                    "`punishment` varchar(255) NOT NULL," +
-                    "`reason` varchar(255) NOT NULL," +
-                    "`date` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP()," +
-                    "PRIMARY KEY (id))");
+        String createOrdersQuery = "CREATE TABLE IF NOT EXISTS orders (" +
+                "`id` int(11) NOT NULL AUTO_INCREMENT," +
+                "`username` varchar(40) NOT NULL," +
+                "`uuid` varchar(60) NOT NULL," +
+                "`user_punished` varchar(40) NOT NULL," +
+                "`punishment` varchar(255) NOT NULL," +
+                "`reason` varchar(255) NOT NULL," +
+                "`date` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP()," +
+                "PRIMARY KEY (id))";
+        String createCommandsQuery = "CREATE TABLE IF NOT EXISTS `commands` (" +
+                "`id` int(11) NOT NULL AUTO_INCREMENT," +
+                "`order_id` int(11) NOT NULL," +
+                "`command` varchar(255) NOT NULL," +
+                "PRIMARY KEY(id)," +
+                "CONSTRAINT FK_orderId FOREIGN KEY (order_id)" +
+                "REFERENCES orders(id) ON DELETE CASCADE)";
+        try(Connection conn = this.hikari.getConnection();
+            PreparedStatement createOrders = conn.prepareStatement(createOrdersQuery);
+            PreparedStatement createCommands = conn.prepareStatement(createCommandsQuery)) {
+
             createOrders.executeUpdate();
-            PreparedStatement createCommands = conn.prepareStatement(
-                    "CREATE TABLE IF NOT EXISTS `commands` (" +
-                            "`id` int(11) NOT NULL AUTO_INCREMENT," +
-                            "`order_id` int(11) NOT NULL," +
-                            "`command` varchar(255) NOT NULL," +
-                            "PRIMARY KEY(id)," +
-                            "CONSTRAINT FK_orderId FOREIGN KEY (order_id)" +
-                            "REFERENCES orders(id) ON DELETE CASCADE)"
-            );
             createCommands.executeUpdate();
             Bukkit.getConsoleSender().sendMessage(ChatColor.GREEN + "MySQL connection established!");
         } catch (SQLException e) {
@@ -79,10 +82,11 @@ public class DatabaseManager {
 
     public List<Order> getOrders() {
         List<Order> orders = new ArrayList<>();
-        try(Connection conn = this.hikari.getConnection()) {
-            String query = "SELECT * FROM orders";
+        String query = "SELECT * FROM orders";
+
+        try(Connection conn = this.hikari.getConnection();
             PreparedStatement st = conn.prepareStatement(query);
-            try (ResultSet rs = st.executeQuery()) {
+            ResultSet rs = st.executeQuery()) {
                 while (rs.next()) {
                     int id = rs.getInt("id");
                     String username = rs.getString("username");
@@ -93,7 +97,6 @@ public class DatabaseManager {
                     Date date = rs.getDate("date");
                     orders.add(new Order(id, username, uuid, userPunished, punishment, reason, date));
                 }
-            }
         } catch (SQLException e) {
             e.printStackTrace();
         }
@@ -101,9 +104,10 @@ public class DatabaseManager {
     }
     public List<String> getCommandsFromOrder(int id) {
         List<String> commands = new ArrayList<>();
-        try(Connection conn = this.hikari.getConnection()) {
-            String query = "SELECT * FROM commands WHERE order_id = ?";
-            PreparedStatement st = conn.prepareStatement(query);
+        String query = "SELECT * FROM commands WHERE order_id = ?";
+
+        try(Connection conn = this.hikari.getConnection();
+            PreparedStatement st = conn.prepareStatement(query)) {
             st.setInt(1, id);
             try (ResultSet rs = st.executeQuery()) {
                 while (rs.next()) {
@@ -117,9 +121,10 @@ public class DatabaseManager {
         return commands;
     }
     public void deleteOrder(int id) {
-        try(Connection conn = this.hikari.getConnection()) {
-            String query = "DELETE FROM orders WHERE id = ?";
-            PreparedStatement st = conn.prepareStatement(query);
+        String query = "DELETE FROM orders WHERE id = ?";
+        
+        try(Connection conn = this.hikari.getConnection();
+            PreparedStatement st = conn.prepareStatement(query)) {
             st.setInt(1, id);
             st.executeUpdate();
         } catch (SQLException e) {
